c7bb64e72633ab62c12cbc5ab09df0ea451c297d
beyondliangcai <www.beyondliangcai@gmail.com>
2021-05-13 19:52:37 +0800

    Update run_glue.sh

--- scripts/run_glue.sh
+++ scripts/run_glue.sh
@@ -2,4 +2,4 @@ export CUDA_VISIBLE_DEVICES=6
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.0/lib64/
 python lsq/main.py --task_name="rte" --data_dir="./glue-data"  --model_dir="finetuned-bert/" --model_save_dir="output_model"  --log_dir="output"
 #run kdlsq glue
-python kdlsq_glue/run_kdlsq_glue.py  --model_dir="finetuned-bert/" --data_dir="./glue-data"--data_dir="./glue-data"
+python kdlsq_glue/run_kdlsq_glue.py  --model_dir="finetuned-bert/" --data_dir="./glue-data"
